7479649ceefb62a39bb63f1ad460a6f38258eaa6
ivanretamozo <retamozoi445@gmail.com>
2022-09-01 16:48:02 -0300

    generando JWT

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,50 @@
 const { response } = require('express');
+const Usuario = require('../models/Usuario');
+const bcrypt = require('bcryptjs');
+const { generarJWT } = require('../helpers/jwt');
 
 //controlador de crear usuario
-const crearUsuario = (req, res = response) => {
+const crearUsuario = async (req, res = response) => {
     const { name, password, email } = req.body;
 
-    res.json({
-        ok: true,
-        msg: 'creando nuevo usuario /new'
-    });
+    try {
+        //verificar que no se repita el email
+        const usuario = await Usuario.findOne({ email });
+
+        if (usuario) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'el email ya se encuentra registrado en la aplicacion'
+            });
+        }
+
+        //crear usuario con el modelo
+        const dbUsuario = new Usuario(req.body);
+
+        //encriptar la contraseña
+        const salt = bcrypt.genSaltSync();
+        dbUsuario.password = bcrypt.hashSync(password, salt);
+
+        //generar JWT
+        const token = await generarJWT(dbUsuario.id, name);
+
+        //creando el usuario en la BD
+        await dbUsuario.save();
+
+        //generar respuesta exitosa
+        return res.status(201).json({
+            ok: true,
+            uid: dbUsuario.id,
+            name,
+            token
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Por Favor,comuniquese con el administrador '
+        });
+    }
 };
 
 //controlador del login de usuario
---
+++ helpers/jwt.js
@@ -0,0 +1,25 @@
+const JWT = require('jsonwebtoken');
+
+const generarJWT = (uid, name) => {
+    const payload = { uid, name };
+    return new Promise((resolve, reject) => {
+        JWT.sign(
+            payload,
+            process.env.SECRET_JWT_SEED,
+            {
+                expiresIn: '24h'
+            },
+            (err, token) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(token);
+                }
+            }
+        );
+    });
+};
+
+module.exports = {
+    generarJWT
+};
